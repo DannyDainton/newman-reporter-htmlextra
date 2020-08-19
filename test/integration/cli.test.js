dfaf7eab16de0618629e51a1df5e367ad80c97db
Danny Dainton <dannydainton@gmail.com>
2020-08-19 22:35:30 +0100

    Fixed an issue with empty `request name / header name` on certain CLI flags

--- test/integration/cli.test.js
+++ test/integration/cli.test.js
@@ -204,10 +204,25 @@ describe('Newman and htmlextra run from the CLI', function () {
                 fs.stat(outFile, done);
             });
     });
-
+    it('should correctly generate the html report when a header is not specified', function (done) {
+        // eslint-disable-next-line max-len
+        exec(`${newman} run test/requests/simple-get-request-with-headers.json -r htmlextra --reporter-htmlextra-export ${outFile} --reporter-htmlextra-skipHeaders`,
+            function (code) {
+                expect(code, 'should have exit code of 0').to.equal(0);
+                fs.stat(outFile, done);
+            });
+    });
     it('should correctly generate the html report for a successful run and remove all headers', function (done) {
         // eslint-disable-next-line max-len
         exec(`${newman} run test/requests/simple-get-request-with-headers.json -r htmlextra --reporter-htmlextra-export ${outFile} --reporter-htmlextra-omitHeaders`,
+            function (code) {
+                expect(code, 'should have exit code of 0').to.equal(0);
+                fs.stat(outFile, done);
+            });
+    });
+    it('should correctly generate the html report when a request name is not specified', function (done) {
+        // eslint-disable-next-line max-len
+        exec(`${newman} run test/requests/simple-get-request.json -r htmlextra --reporter-htmlextra-export ${outFile} --reporter-htmlextra-hideResponse`,
             function (code) {
                 expect(code, 'should have exit code of 0').to.equal(0);
                 fs.stat(outFile, done);
